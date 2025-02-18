24902ca0da33ada5bfdd75656420d966c02632b6
jleenerts <jacobleenerts97+code@gmail.com>
2025-02-18 00:25:51 -0800

    map implementation, not working

--- problems/twoSum/j/twosum.cpp
+++ problems/twoSum/j/twosum.cpp
@@ -1,24 +1,30 @@
 #include "twosum.h"
+#include <unordered_map>
+using namespace std;
 
-std::vector<int> TwoSum::twoSum(const std::vector<int>& nums, int target) {
-    std::vector<int> result;
+vector<int> TwoSum::twoSum(const vector<int>& nums, int target) {
+    vector<int> result;
 
-    // the outer loop does not need to run to the last element
-    // as there is no further elements to compare itself to
-    std::size_t outerLoopSize = nums.size() - 1;
-
-    for (std::size_t i = 0; i < outerLoopSize; i++) {
-        for (std::size_t j = i + 1; j < nums.size(); j++) {
-            if (nums[i] + nums[j] == target) {
-                result.push_back(i);
-                result.push_back(j);
-                return result;
-            }
+    // we can use a hash map to store the values we have seen
+    // first value is the difference and second value is the position of the number
+    
+    unordered_map<int, int> seen;
+    for (size_t i = 0; i < nums.size(); i++) {
+        // difference is subtracting the current number from the target
+        int difference = target - nums[i];
+        // if it is already in the map, we have found the solution
+        if (seen.find(difference) != seen.end()) {
+            result = { seen[difference], static_cast<int>(i) };
+            return result;
+        }
+        // else we add the current number to the map
+        else {
+            seen[difference] = i;
         }
     }
 
     // we are allowed to assume the solution is within array but this is
     // a simple result to return if no solution is found
-    result = {-1, -1};
+    result = { -1, -1 };
     return result;
 }
