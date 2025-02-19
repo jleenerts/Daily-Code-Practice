96667afa3200acf27f1a614c53a26ed1577b21ce
jleenerts <jacobleenerts97+code@gmail.com>
2025-02-18 17:21:19 -0800

    set up groupAnagrams

---
+++ problems/groupAnagrams/j/groupGrams.cpp
@@ -0,0 +1,10 @@
+#include "groupGrams.h"
+
+using namespace std;
+
+vector<vector<string>> GroupAnagrams::groupAnagrams(const vector<string>& data) {
+    int i = data.size();
+    i++;
+
+    return vector<vector<string>>();
+}
---
+++ problems/groupAnagrams/j/groupGrams.h
@@ -0,0 +1,17 @@
+#ifndef GROUPGRAMS_H
+#define GROUPGRAMS_H
+
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class GroupAnagrams {
+    public:
+        vector<vector<string>> groupAnagrams(const vector<string>& strs);
+};
+
+
+
+#endif
