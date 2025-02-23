117fd2ecd98910541fa4958ad8f703fbb1c66f10
MichaelPhilbin <michaelphilbinn@gmail.com>
2025-02-22 22:36:21 -0800

    v2 median of two lists completed

--- problems/medianTwoLists/m/median.cpp
+++ problems/medianTwoLists/m/median.cpp
@@ -1,71 +1,35 @@
 #include "median.h"
 
-double median::findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2)
-{
-    int size = nums1.size() + nums2.size();
+double median::findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+    if (nums1.size() > nums2.size()) 
+        return findMedianSortedArrays(nums2, nums1); 
 
-    if (nums1.size() == 0) 
-    {
-        if (size % 2 == 0)
-            return (nums2[(size-1)/2] + nums2[((size-1)/2)+1]) / 2.0;
-        else return nums2[(size-1)/2];
-    }
-    
-    if (nums2.size() == 0)
-    {
-        if (size % 2 == 0)
-            return (nums1[(size-1)/2] + nums1[((size-1)/2)+1]) / 2.0;
-        else return nums1[(size-1)/2];
-    }
+    int m = nums1.size(), n = nums2.size();
+    int left = 0, right = m;
+    int medianPos = (m + n + 1) / 2;
 
-    if (size % 2 == 0) // even
-    {
-        int toFind = (size-1)/2;
-        
-        int i = 0;
-        int i1 = 0;
-        int i2 = 0;
-        while(i1 < nums1.size() || i2 < nums2.size())
-        {
-            if (i == toFind)
-            {
-                if (nums1[i1] < nums2[i2])
-                {
-                    int calc = nums1[i1];
-                    if (i1+1 < nums1.size()) 
-                        i1++;
-                    else 
-                        return (calc + nums2[i2]) / 2.0;
-                  
-                    calc += (nums1[i1] < nums2[i2]) ? nums1[i1] : nums2[i2];
-                    return calc/2.0;
-                }
-                return (nums1[i1] + nums2[i2]) / 2.0;
+    while (left <= right) {
+        int partition1 = (left + right) / 2;
+        int partition2 = medianPos - partition1;
+
+        int maxLeft1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
+        int minRight1 = (partition1 == m) ? INT_MAX : nums1[partition1];
+
+        int maxLeft2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
+        int minRight2 = (partition2 == n) ? INT_MAX : nums2[partition2];
+
+        if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+            if ((m + n) % 2 == 0) {
+                return (std::max(maxLeft1, maxLeft2) + std::min(minRight1, minRight2)) / 2.0;
+            } else {
+                return std::max(maxLeft1, maxLeft2);
             }
-                
-
-            if (i1+1 < nums1.size() && nums1[i1] < nums2[i2]) i1++;
-            else if (i2+1 < nums2.size() && nums1[i1] > nums2[i2]) i2++;
-
-            if (i1+1 < nums1.size() && nums1[i1] == nums2[i2]) i1++;
-            else if (nums1[i1] == nums2[i2]) i2++;
-
-            i++;
+        } else if (maxLeft1 > minRight2) {
+            right = partition1 - 1;
+        } else {
+            left = partition1 + 1;
         }
-    }else             
-    {
-        int toFind = size/2;
-        
-        int i = 0, i1 = 0, i2 = 0;
-        while(i1 < nums1.size() || i2 < nums2.size())
-        {
-            if (i == toFind)
-                return nums1[i1] < nums2[i2] ? nums1[i1] : nums2[i2];
-
-            if (i1 < nums1.size() && nums1[i1] < nums2[i2]) i1++;
-            else if (i2 < nums2.size() && nums1[i1] > nums2[i2]) i2++;
-            i++;     
-        }
-
     }
+
+    return 0.0;
 }
