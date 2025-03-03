b48c9000a9ab53b95a068963a41fa4441d92c345
MichaelPhilbin <michaelphilbinn@gmail.com>
2025-03-02 23:33:24 -0800

    starting an auto problem folder generator

---
+++ createPF/createProblemFolder.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {  
+    std::cout << "Please enter Problem Name: " << std::endl;
+    std::string problemName = "";
+
+    std::cout << "Please enter Subfolder(m/j/both) Name: " << std::endl;
+    char SubfolderChar = 'm';
+
+    return 0;
+}
